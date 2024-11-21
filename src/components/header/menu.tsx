3b8c956ae19da03278ee7de054064a6b3b0fa4b3
王玉朝 <wangyuchaogeek@163.com>
2024-11-21 11:34:37 +0800

    toml

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -5,9 +5,7 @@ function ThemeToggle() {
   return (
     <li onClick={toggleDark}>
       <span className={$("inline-block", isDark ? "i-ph-moon-stars-duotone" : "i-ph-sun-dim-duotone")} />
-      <span>
-        {isDark ? "黑暗模式" : "白天模式"}
-      </span>
+      <span>{isDark ? "黑暗模式" : "白天模式"}</span>
     </li>
   )
 }
@@ -18,31 +16,26 @@ export function Menu() {
   return (
     <span className="relative" onMouseEnter={() => show(true)} onMouseLeave={() => show(false)}>
       <span className="flex items-center scale-90">
-        {
-          enableLogin && loggedIn && userInfo.avatar
-            ? (
-                <button
-                  type="button"
-                  className="h-6 w-6 rounded-full bg-cover"
-                  style={
-                    {
-                      backgroundImage: `url(${userInfo.avatar})`,
-                    }
-                  }
-                >
-                </button>
-              )
-            : <button type="button" className="btn i-si:more-muted-horiz-circle-duotone" />
-        }
+        {enableLogin && loggedIn && userInfo.avatar
+          ? (
+              <button
+                type="button"
+                className="h-6 w-6 rounded-full bg-cover"
+                style={{
+                  backgroundImage: `url(${userInfo.avatar})`,
+                }}
+              >
+              </button>
+            )
+          : (
+              <button type="button" className="btn i-si:more-muted-horiz-circle-duotone" />
+            )}
       </span>
       {shown && (
         <div className="absolute right-0 z-99 bg-transparent pt-4 top-4">
           <motion.div
             id="dropdown-menu"
-            className={$([
-              "w-200px",
-              "bg-primary backdrop-blur-5 bg-op-70! rounded-lg shadow-xl",
-            ])}
+            className={$(["w-200px", "bg-primary backdrop-blur-5 bg-op-70! rounded-lg shadow-xl"])}
             initial={{
               scale: 0.9,
             }}
@@ -51,7 +44,8 @@ export function Menu() {
             }}
           >
             <ol className="bg-base bg-op-70! backdrop-blur-md p-2 rounded-lg color-base text-base">
-              {enableLogin && (loggedIn
+              {enableLogin
+              && (loggedIn
                 ? (
                     <li onClick={logout}>
                       <span className="i-ph:sign-out-duotone inline-block" />
@@ -70,17 +64,13 @@ export function Menu() {
                 <span>Star on Github </span>
               </li>
               <li className="flex gap-2 items-center">
-                <a
-                  href="https://github.com/ourongxing/newsnow"
-                >
+                <a href="https://github.com/ourongxing/newsnow">
                   <img
                     alt="GitHub stars badge"
                     src="https://img.shields.io/github/stars/ourongxing/newsnow?logo=github"
                   />
                 </a>
-                <a
-                  href="https://github.com/ourongxing/newsnow/fork"
-                >
+                <a href="https://github.com/ourongxing/newsnow/fork">
                   <img
                     alt="GitHub forks badge"
                     src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github"
